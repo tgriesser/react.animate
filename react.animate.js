373825a3d4a1b62062dc954513cc7710f7423cc8
Tim Griesser <tgriesser10@gmail.com>
2014-09-19 17:39:30 -0400

    Use bower path to main ease.js project

--- react.animate.js
+++ react.animate.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ease'], factory);
+    define(['ease-component'], factory);
   } else if (typeof exports === 'object') {
     module.exports = factory(require('ease-component'));
   } else {
